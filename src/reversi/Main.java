a0b6fbe4e3de2352ce9a5e56169a52b64432b1b7
psypc1 <psypc1@nottingham.ac.uk>
2021-03-20 21:24:10 +0530

    First

--- src/reversi/Main.java
+++ src/reversi/Main.java
@@ -1,8 +1,7 @@
 package reversi;
 
 public class Main {
-    public int i;
     public static void main(String[] args){
-        System.out.println("Hello World");
+        new Board(new Game());
     }
 }
